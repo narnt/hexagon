622c966e26c0f8db86cadb6e24f9fff0831c8c4f
narnt <32932910+narnt@users.noreply.github.com>
2017-12-05 16:23:00 +0300

    Add files via upload

---
+++ hexa/hexa.js
@@ -0,0 +1,54 @@
+function polygon(nodeId, deep) {
+    let z = 0;
+    let n = 0;
+    let m = 0;
+    let l = 0;
+    for (i = 0; i < 28; i++) {
+        let toClone = document.querySelector(nodeId);
+        let clonedNode = toClone.cloneNode(deep);
+        let insertPoint = document.getElementById('inserthere');
+        let newEl = insertPoint.appendChild(clonedNode);
+        newEl.removeAttribute('id');
+        newEl.setAttribute('class', i);
+        let x = i * 90;
+        if (x < 500) {
+            newEl.style.left = x + 'px';
+        } else {
+            y = 80;
+            x = z * 90 + 45;
+            newEl.style.top = y + 'px';
+            newEl.style.left = x + 'px';
+            z++;
+            if (x < 450) {
+                newEl.style.left = x + 'px';
+            } else {
+                y = 160;
+                x = n * 90;
+                newEl.style.top = y + 'px';
+                newEl.style.left = x + 'px';
+                n++;
+                if (x < 500) {
+                    newEl.style.left = x + 'px';
+                } else {
+                    y = 240;
+                    x = m * 90 + 45;
+                    newEl.style.top = y + 'px';
+                    newEl.style.left = x + 'px';
+                    m++;
+                    if (x < 450) {
+                        newEl.style.left = x + 'px';
+                    } else {
+                        y = 320;
+                        x = l * 90;
+                        newEl.style.top = y + 'px';
+                        newEl.style.left = x + 'px';
+                        l++;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+polygon('#poly', true);
